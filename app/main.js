373df2acf860498bf1c3a273660ddd1ed6299945
sabazusi <kouhei.live@gmail.com>
2016-05-20 02:09:39 +0900

    add initial template.

--- app/main.js
+++ app/main.js
@@ -3,8 +3,21 @@ const BrowserWindow = require('electron').BrowserWindow;
 
 app.on('ready', () => {
   console.log('hai');
-  const w = new BrowserWindow({
+  const loginWindow = new BrowserWindow({
     width: 300, height: 300, show: false
   });
-  w.loadURL('file://' + __dirname + '/main.html');
+  const settingWindow = new BrowserWindow({
+    width: 300, height: 300, show: false
+  });
+  // w.loadURL('file://' + __dirname + '/main.html');
+
+  // load localStorage from login window.
+  loginWindow.loadURL('file://' + __dirname + '/login.html');
+
+  // if logged in succeeded with saved key
+  // -> remove login window. start setting window.
+  
+  // if loggedin failed or does not exist saved key
+  // -> open login form(after login suceeded, save key and start setting window.) 
+  // loginWindow.show = true;
 });
