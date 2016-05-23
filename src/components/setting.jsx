60b8ce3beb0d1d5452d1ba08cf85c944de35ca76
sabazusi <kouhei.live@gmail.com>
2016-05-24 00:47:37 +0900

    add component

---
+++ src/components/setting.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Setting extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        あああああああああああああ
+      </div>
+    )
+  }
+}
--- src/setting.js
+++ src/setting.js
@@ -1,9 +1,18 @@
 import { ipcRenderer } from 'electron';
 import NicoSessionClient from './client/nicosession-client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Setting from './components/setting';
 
 window.onload = () => {
   ipcRenderer.on('loginSucceeded', (e, keys) => {
     localStorage.setItem('login.email', keys.email)
     localStorage.setItem('login.password', keys.password)
+    rendering();
   });
 };
+
+
+function rendering() {
+  ReactDOM.render(<Setting/>, document.getElementById('root'))
+}
