9fb05544453e79095b14e2c4c6f178e899b9b01b
sabazusi <kouhei.live@gmail.com>
2016-05-28 00:59:13 +0900

    scrape communities(only 1page)

--- src/client/community-client.js
+++ src/client/community-client.js
@@ -12,8 +12,19 @@ class CommunityClient {
         throw 'login failed';
       }
     }).then((result) => {
-      console.log(result);
+      const dom = result.$('div.com_frm');
+      const communities =  Object.keys(dom).map((key)=> {
+        try {
+          const com = dom[`${key}`].children[0].children[0].children[1].children[1].children[0];
+          return {title: com.children[0].data, comid: com.attribs.href.replace('/community/', '')};
+        } catch(e) {
+          return undefined;
+        }
+      }).filter((e) => {return e != undefined;});
+      console.log(communities);
+      return communities;
     }).catch((e) => {
+      return [];
     });
   }
 }
