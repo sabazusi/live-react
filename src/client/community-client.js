589b2007f05376353b8d2177383ebf506bf0f5de
sabazusi <kouhei.live@gmail.com>
2016-06-01 00:34:25 +0900

    devide function.

--- src/client/community-client.js
+++ src/client/community-client.js
@@ -7,24 +7,30 @@ class CommunityClient {
     }).then((result) => {
       // login succeeded if result.cookies.user_session exist.
       if(result.response.cookies.user_session){
-        return client.fetch('http://com.nicovideo.jp/community');
+        return this._getCommunities(result);
       } else {
         throw 'login failed';
       }
-    }).then((result) => {
-      const dom = result.$('div.com_frm');
-      const communities =  Object.keys(dom).map((key)=> {
-        try {
-          const com = dom[`${key}`].children[0].children[0].children[1].children[1].children[0];
-          return {title: com.children[0].data, comid: com.attribs.href.replace('/community/', '')};
-        } catch(e) {
-          return undefined;
-        }
-      }).filter((e) => {return e != undefined;});
-      return communities;
     }).catch((e) => {
       return [];
     });
+  }
+
+  _getCommunities(result) {
+    return client.fetch('http://com.nicovideo.jp/community')
+      .then((result) => {
+        const dom = result.$('div.com_frm');
+        const communities =  Object.keys(dom).map((key)=> {
+          try {
+            const com = dom[`${key}`].children[0].children[0].children[1].children[1].children[0];
+            return {title: com.children[0].data, comid: com.attribs.href.replace('/community/', '')};
+          } catch(e) {
+            return undefined;
+          }
+        }).filter((e) => {return e != undefined;});
+        return communities;
+      }
+    );
   }
 }
 
