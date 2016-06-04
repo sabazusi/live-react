a9c22c704e3fe3f62f14041e6764a9a71e9cac5b
sabazusi <kouhei.live@gmail.com>
2016-06-05 00:31:58 +0900

    add xstream

--- src/client/livealert-client.js
+++ src/client/livealert-client.js
@@ -1,4 +1,5 @@
 import xml2js from 'xml2js';
+import xs from 'xstream';
 
 class LiveAlertClient {
   start() {
@@ -13,6 +14,27 @@ class LiveAlertClient {
           const thread = result.getalertstatus.ms[0].thread;
         });
       });
+  }
+
+  getStream() {
+    return xs.create({
+      id: 0,
+      stop: () => {}, // TODO: do anything.
+      start: (listener) => {
+        fetch('http://live.nicovideo.jp/api/getalertinfo')
+          .then((response) => {
+            return response.text()
+          })
+          .then((body) => {
+            xml2js.parseString(body, (err, result) => {
+              const url = result.getalertstatus.ms[0].addr;
+              const port = result.getalertstatus.ms[0].port;
+              const thread = result.getalertstatus.ms[0].thread;
+            });
+            listener.next('alert info.');
+          });
+      }
+    });
   }
 }
 
